cca4ddea72c9a5643565fc335fff0c5f0b5e85bd
benjamin-abebe <benjamin.abebe@mths.ca>
2024-09-27 10:48:21 -0400

    Changed comments

--- main.ts
+++ main.ts
@@ -1,8 +1,23 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2024 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Benjamin
+ * Created on: Sep 2024
+ * This program turns an LED on a breadboard on and off.
 */
 
-basic.showString('Hello, World!')
+// setup
+basic.clearScreen()
+pins.digitalWritePin(DigitalPin.P16, 0)
+basic.showIcon(IconNames.Happy)
+
+// turn on LED
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Yes)
+    pins.digitalWritePin(DigitalPin.P16, 1)
+})
+
+// turn off LED
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.No)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+})
